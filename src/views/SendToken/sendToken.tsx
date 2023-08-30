50649d6b7a317d64577bfa4f665e74329732e551
levtea <rzh2000@163.com>
2023-08-30 09:18:51 +0800

    fix

--- src/views/SendToken/sendToken.tsx
+++ src/views/SendToken/sendToken.tsx
@@ -13,22 +13,14 @@ import { formatWeiToEth } from '@/utils/formatterEth';
 // import SendApproveDialog from '@/components/TokensOverview/sendApprove';
 // import { useNavigate } from 'react-router-dom';
 import SwitchPaymasterDialog from '@/components/TokensOverview/switchPaymaster';
+import { GetAccountAsset } from '@/actions/Token/token';
+import { getCurrentAddress } from '@/utils/localStorage';
+import { GetUser } from '@/actions/User/user';
+import { setUserRecoverEmail } from '@/utils/localStorage';
 
 // import { getSendTransactionType } from '@/utils/localStorage';
 
 import '@/assets/styles/accountStyle/style.scss';
-
-const contentStyle: React.CSSProperties = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '60px',
-  width: '100%',
-  color: '#000000',
-  marginTop: 15,
-  paddingLeft: 15,
-  paddingRight: 15,
-};
 
 const addressStyle: React.CSSProperties = {
   display: 'flex',
@@ -50,6 +42,7 @@ const View = () => {
   // const handleSendApproveFlagClose = () => {
   //   setSendApproveFlag(false);
   // };
+  const [isInputAddress, setIsInputAddress] = useState(true);
 
   const [switchPaymentFlag, setSwitchPaymentFlag] = useState(false);
   const handleSwitchPaymentFlagClose = () => {
@@ -67,58 +60,86 @@ const View = () => {
     setToAddress(address);
   };
 
-  // const send = async () => {
-  //   setIsLoading(true);
-  //   if (!search.get('tokenAddress')) {
-  //     const sendRes = await SendNativeToken(toAddress, toAmount.trim());
-  //     console.log('sendRes', sendRes);
-  //     if (sendRes.code === 200) {
-  //       messageApi.success('Complete');
-  //     } else if (sendRes.code === 428) {
-  //       setSendApproveFlag(true);
-  //       setPaymasterAddress(sendRes.data.PaymasterAddress);
-  //       setErc20Address(sendRes.data.Erc20ContractAddress);
-  //     } else {
-  //       messageApi.error('Fail');
-  //     }
-  //   } else {
-  //     const data =
-  //       '0xa9059cbb000000000000000000000000' +
-  //       toAddress.trim().slice(2) +
-  //       '0'.repeat(64 - ethers.parseEther(toAmount).toString(16).length) +
-  //       ethers.parseEther(toAmount).toString(16);
-  //     console.log('data', data);
-  //     console.log('data1Length', data.length);
-  //     const sendRes = await SendErc20Token(search.get('tokenAddress') as string, data);
-  //     console.log('sendRes', sendRes);
-  //     if (sendRes.code === 200) {
-  //       messageApi.success('Complete');
-  //       navigateTo('/overview');
-  //     } else if (sendRes.code === 428) {
-  //       setSendApproveFlag(true);
-  //       setPaymasterAddress(sendRes.data.PaymasterAddress);
-  //       setErc20Address(sendRes.data.Erc20ContractAddress);
-  //     } else {
-  //       messageApi.error('Fail');
-  //     }
-  //   }
-  //   setIsLoading(false);
-  // };
-
   const payment = () => {
     setIsLoading(true);
     setSwitchPaymentFlag(true);
     setIsLoading(false);
   };
 
+  const loadAccountData = async () => {
+    // account
+    const res = await GetAccountAsset();
+    // const addressSet = new Set();
+    if (res.code === 200) {
+      AccountStore.clearAccountList();
+      // AccountStore.clearCurrentAccount();
+      if (res.data.abstract_account) {
+        AccountStore.pushAccount({
+          address: res.data.abstract_account.Address,
+          erc20AccountMap: res.data.abstract_account.Erc20,
+          nativeBalance: res.data.abstract_account.Native,
+          isMultisig: false,
+          isUpdate: false,
+          name: res.data.abstract_account.Name,
+        });
+        console.log('pushAccount_abstract_account', {
+          address: res.data.abstract_account.Address,
+          erc20AccountMap: res.data.abstract_account.Erc20,
+          nativeBalance: res.data.abstract_account.Native,
+          isMultisig: false,
+          isUpdate: false,
+          name: res.data.abstract_account.Name,
+        });
+      }
+      if (res.data.multiple_abstract_account) {
+        res.data.multiple_abstract_account.map((item) => {
+          // if (!addressSet.has(item.Address)) {
+          AccountStore.pushAccount({
+            address: item.Address,
+            erc20AccountMap: item.Erc20,
+            nativeBalance: item.Native,
+            isMultisig: true,
+            isUpdate: false,
+            name: item.Name,
+          });
+          // }
+          // addressSet.add(item.Address);
+        });
+      }
+
+      if (getCurrentAddress()) {
+        const currentWalletAddress = AccountStore.getAccountByAddress(getCurrentAddress());
+        console.log('currentWalletAddress', currentWalletAddress.address);
+        console.log('getCurrentAddress', getCurrentAddress());
+        if (currentWalletAddress.address) {
+          AccountStore.setCurrentAccount(currentWalletAddress);
+        }
+      } else {
+        AccountStore.setCurrentAccount({
+          address: res.data.abstract_account.Address,
+          erc20AccountMap: res.data.abstract_account.Erc20,
+          nativeBalance: res.data.abstract_account.Native,
+          isMultisig: false,
+          isUpdate: false,
+          name: res.data.abstract_account.Name,
+        });
+      }
+    }
+    const userRes = await GetUser();
+    console.log('GetUser res', userRes);
+    if (userRes.code === 200) {
+      console.log('res.data.recover_email', userRes.data.recover_email);
+      setUserRecoverEmail(userRes.data.recover_email);
+    }
+  };
+
+  useEffect(() => {
+    // load
+    loadAccountData();
+  }, []);
+
   return (
     <div>
-      {/* <SendApproveDialog
-        isOpen={sendApproveFlag}
-        onClose={handleSendApproveFlagClose}
-        paymentAddress={paymasterAddress}
-        erc20Address={erc20Address}
-      /> */}
       <SwitchPaymasterDialog
         isOpen={switchPaymentFlag}
         onClose={handleSwitchPaymentFlagClose}
@@ -126,76 +147,210 @@ const View = () => {
         toAddress={toAddress}
         erc20Address={search.get('tokenAddress') || ''}
       />
-      <div style={contentStyle}>
+      <div style={{ color: '#000000', marginTop: 20 }}>
         <span style={{ fontSize: 18 }}>Send To</span>
-        <Link to="/overview" style={{ marginLeft: '230px', fontSize: 18 }}>
-          Back
-        </Link>
+        {isInputAddress ? (
+          <Link to="/overview" style={{ marginLeft: '230px', fontSize: 18 }}>
+            Back
+          </Link>
+        ) : (
+          <Button
+            type="link"
+            style={{ marginLeft: '230px', fontSize: 18 }}
+            onClick={() => {
+              setIsInputAddress(true);
+            }}>
+            Back
+          </Button>
+        )}
       </div>
-      <div style={contentStyle}>
-        <Input
-          placeholder="Enter To Address"
-          value={toAddress}
-          onChange={(e) => {
-            if (e != null) {
-              setToAddress(e.target.value);
-            }
-          }}
-        />
-      </div>
-      <div style={contentStyle}>
-        <Input
-          placeholder="Enter Amount"
-          onChange={(e) => {
-            if (e != null) {
-              setToAmount(e.target.value);
-            }
-          }}
-        />
-      </div>
-      <div style={contentStyle}>
-        <Button type="primary" size="large" loading={isLoading} onClick={payment}>
-          Send
-        </Button>
-      </div>
-      <div style={addressStyle}>
-        <div style={{ height: 400, marginTop: 10, overflowY: 'auto', width: '100%', color: '#000000' }}>
-          <span>Your accounts:</span>
-          {AccountStore.accountList.map((row, index) => (
-            <div
-              className={classNames(
-                'accountContentSend',
-                row.address === AccountStore.currentAccount.address ? 'accountContentSelectSend' : 'accountContentSend'
-              )}
-              key={index}
-              style={{ padding: 10, marginTop: 30 }}
-              onClick={() => {
-                selectAccount(row.address);
-              }}>
-              <Space direction="vertical" size="large" style={{ display: 'flex' }}>
-                <Row>
-                  <Col span={24} style={{ display: 'flex', flexDirection: 'row' }}>
-                    <span style={{ fontSize: '15px', fontWeight: 'bold', marginLeft: 45 }}>
-                      {row.name ? row.name : 'Account'}
-                      {row.isMultisig ? '(Multisig)' : '(Abstract)'}
-                    </span>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col span={16}>
-                    <span>{truncateWalletAddrLong(row.address)}</span>
-                  </Col>
-                  <Col span={8}>
-                    <span style={{ textAlign: 'right' }}>
-                      {formatWeiToEth(row.nativeBalance)} {' ' + AccountStore.getCurrentNetworkWithStorage().symbol}
-                    </span>
-                  </Col>
-                </Row>
-              </Space>
+
+      {isInputAddress ? (
+        <div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%',
+              color: '#000000',
+              padding: 10,
+              marginTop: 30,
+            }}>
+            <Input
+              style={{ height: 50 }}
+              placeholder="Enter To Address"
+              value={toAddress}
+              onChange={(e) => {
+                if (e != null) {
+                  setToAddress(e.target.value);
+                }
+              }}
+            />
+          </div>
+          <div style={addressStyle}>
+            <div style={{ height: 500, marginTop: 10, overflowY: 'auto', width: '100%', color: '#000000' }}>
+              <span style={{ display: 'flex', marginLeft: 20, fontWeight: 'bolder', fontSize: 20 }}>
+                Your accounts:
+              </span>
+              {AccountStore.accountList.map((row, index) => (
+                <div
+                  className={classNames(
+                    'accountContentSend',
+                    row.address === AccountStore.currentAccount.address
+                      ? 'accountContentSelectSend'
+                      : 'accountContentSend'
+                  )}
+                  key={index}
+                  style={{ padding: 10, marginTop: 30 }}
+                  onClick={() => {
+                    selectAccount(row.address);
+                  }}>
+                  <Row>
+                    <Col span={5} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                      <div
+                        style={{
+                          width: 47,
+                          height: 47,
+                          borderRadius: '50%',
+                          background: 'linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)',
+                        }}></div>
+                    </Col>
+                    <Col span={19}>
+                      <Space direction="vertical" size="large" style={{ display: 'flex' }}>
+                        <Row>
+                          <Col span={24} style={{ display: 'flex', flexDirection: 'row' }}>
+                            <span style={{ fontSize: '15px', fontWeight: 'bold' }}>
+                              {row.name ? row.name : 'Account'}
+                              {row.isMultisig ? '(Multisig)' : '(Abstract)'}
+                            </span>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col span={16} style={{ display: 'flex' }}>
+                            <span>{truncateWalletAddrLong(row.address)}</span>
+                          </Col>
+                          <Col span={8}>
+                            <span style={{ textAlign: 'right' }}>
+                              {formatWeiToEth(row.nativeBalance)}{' '}
+                              {' ' + AccountStore.getCurrentNetworkWithStorage().symbol}
+                            </span>
+                          </Col>
+                        </Row>
+                      </Space>
+                    </Col>
+                  </Row>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
+          <div style={{ padding: 10 }}>
+            <Button
+              type="primary"
+              size="large"
+              style={{ width: '100%' }}
+              onClick={() => {
+                setIsInputAddress(false);
+              }}>
+              Next
+            </Button>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div style={{ color: '#000000', paddingLeft: 10, paddingRight: 10, marginTop: 40 }}>
+          <div>
+            <div style={{ backgroundColor: '#E6F0FA', padding: 5, marginTop: 20, borderRadius: '15px' }}>
+              {AccountStore.currentAccount.address && (
+                <>
+                  <span style={{ padding: 10, display: 'flex', color: '#000000' }}>
+                    {AccountStore.currentAccount.name ? AccountStore.currentAccount.name : 'Account'}
+                  </span>
+                  <div
+                    style={{
+                      width: '100%',
+                      color: '#000000',
+                      height: 20,
+                    }}>
+                    {AccountStore.currentAccount.address}
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+
+          <Row style={{ marginTop: 20 }}>
+            <Col span={8} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <span style={{ fontSize: 18 }}>Asset:</span>
+            </Col>
+            <Col span={16}>
+              <Row>
+                <Space direction="vertical" size="small" style={{ display: 'flex' }}>
+                  <Col span={24} style={{ textAlign: 'left' }}>
+                    {search.get('tokenAddress')
+                      ? AccountStore.currentAccount.erc20AccountMap[search.get('tokenAddress') || ''].name
+                      : AccountStore.getCurrentNetworkSymbol()}
+                  </Col>
+                  <Col span={24} style={{ textAlign: 'left' }}>
+                    Balance:
+                    {search.get('tokenAddress')
+                      ? AccountStore.currentAccount.erc20AccountMap[search.get('tokenAddress') || ''] &&
+                        formatWeiToEth(
+                          AccountStore.currentAccount.erc20AccountMap[search.get('tokenAddress') || ''].balance
+                        )
+                      : formatWeiToEth(AccountStore.currentAccount.nativeBalance) +
+                        ' ' +
+                        AccountStore.getCurrentNetworkSymbol()}
+                  </Col>
+                </Space>
+              </Row>
+            </Col>
+          </Row>
+
+          <Row style={{ marginTop: 50 }}>
+            <Col span={8} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <Space direction="vertical" size="small" style={{ display: 'flex' }}>
+                <Col span={24} style={{ fontSize: 18 }}>
+                  Amount:
+                </Col>
+                <Col span={24}>
+                  <Button
+                    type="link"
+                    size="small"
+                    onClick={() => {
+                      const amount = search.get('tokenAddress')
+                        ? AccountStore.currentAccount.erc20AccountMap[search.get('tokenAddress') || ''] &&
+                          formatWeiToEth(
+                            AccountStore.currentAccount.erc20AccountMap[search.get('tokenAddress') || ''].balance
+                          )
+                        : formatWeiToEth(AccountStore.currentAccount.nativeBalance);
+                      setToAmount(amount);
+                    }}>
+                    Max
+                  </Button>
+                </Col>
+              </Space>
+            </Col>
+            <Col span={16}>
+              <Input
+                style={{ height: 50 }}
+                placeholder="Enter Amount"
+                value={toAmount}
+                onChange={(e) => {
+                  if (e != null) {
+                    setToAmount(e.target.value);
+                  }
+                }}
+              />
+            </Col>
+          </Row>
+
+          <div style={{ padding: 10, marginTop: 70 }}>
+            <Button style={{ width: '100%' }} type="primary" size="large" loading={isLoading} onClick={payment}>
+              Send
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
