f56e2595267a8498fe7b438618da3271407cfbc1
levtea <rzh2000@163.com>
2023-08-31 19:56:13 +0800

    fix

--- src/components/TokensOverview/switchPaymaster.tsx
+++ src/components/TokensOverview/switchPaymaster.tsx
@@ -48,7 +48,7 @@ const SwitchPaymasterDialog = ({
         setTimeout(() => navigateTo('/overview'), 700);
         messageApi.success('Complete');
       } else {
-        messageApi.error(sendRes.data);
+        messageApi.error(sendRes.data.ErrorMessage);
       }
     } else {
       const data =
@@ -64,7 +64,7 @@ const SwitchPaymasterDialog = ({
         setTimeout(() => navigateTo('/overview'), 700);
         messageApi.success('Complete');
       } else {
-        messageApi.error(sendRes.data);
+        messageApi.error(sendRes.data.ErrorMessage);
       }
     }
     setIsLoading(false);
